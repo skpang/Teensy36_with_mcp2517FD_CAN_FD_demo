2dde3afa0096a2ffe9df13b7c7a9933d729e0810
Sukkin Pang <skp@skpang.co.uk>
2018-01-01 17:12:30 +0000

    Initial commit

---
+++ Teensy36_with_MCP2517FD_CAN_FD_demo.ino
@@ -0,0 +1,143 @@
+/*
+ * Teensy 3.2 with MCP2517FD CAN FD demo
+ * http://skpang.co.uk/catalog/mcp2517fd-can-fd-breakout-with-teensy-include-teensy-32-p-1549.html
+ * 
+ * Optional OLED display:
+ * http://skpang.co.uk/catalog/oled-128x64-display-for-teensy-breakout-board-p-1508.html
+ * 
+ * v1.0 December 2017
+ * 
+ * skpang.co.uk
+ * 
+ */
+
+
+#include "drv_canfdspi_api.h"
+#include "drv_canfdspi_register.h"
+#include "drv_canfdspi_defines.h"
+#include "drv_spi.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+volatile uint32_t can_msg_count = 0;
+
+extern uint8_t txd[MAX_DATA_BYTES];
+extern CAN_TX_MSGOBJ txObj;
+
+#define OLED_RESET 5
+Adafruit_SSD1306 display(OLED_RESET);
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+
+const int JOY_UP = 22; 
+const int JOY_CLICK = 17; 
+const int JOY_DOWN = 23; 
+const int JOY_LEFT = 20; 
+const int JOY_RIGHT = 21; 
+
+void setup() {
+
+  pinMode(JOY_UP, INPUT_PULLUP);
+  pinMode(JOY_CLICK, INPUT_PULLUP);
+  pinMode(JOY_DOWN, INPUT_PULLUP);
+  pinMode(JOY_LEFT, INPUT_PULLUP);
+  pinMode(JOY_RIGHT, INPUT_PULLUP);
+  
+  delay(1000);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.display();
+  display.clearDisplay();
+  display.setTextSize(0);
+  display.setTextColor(WHITE);
+  display.setCursor(0,15);
+  display.println(" Teensy FD Demo");
+  display.println(" ");
+  display.println(" skpang.co.uk");
+  display.println(" ");
+  display.println(" 121/17");
+  display.display();
+      
+  Serial.println(F("CAN Bus Tx test"));
+ 
+  //APP_CANFDSPI_Init(CAN_500K_2M);       // 500k arbitration rate and 2Mbps data rate
+  // APP_CANFDSPI_Init(CAN_500K_4M);       // 500k arbitration rate and 2Mbps data rate
+   APP_CANFDSPI_Init(CAN_500K_8M);       // 500k arbitration rate and 2Mbps data rate
+ 
+  
+  txObj.bF.ctrl.IDE = 0;      // Extended CAN ID false
+  txObj.bF.id.SID = 0x7df;    // CAN ID
+  txObj.bF.ctrl.BRS = 1;      // Switch Bitrate true (switch to 2Mbps)
+  txObj.bF.ctrl.FDF = 1;      // CAN FD true
+  txObj.bF.ctrl.DLC = 15;      // Data length
+  txd[4] = can_msg_count >> 24;
+  txd[5] = can_msg_count >> 16;
+ txd[6] = can_msg_count >> 8;
+ txd[7] = can_msg_count;
+ delay(100);
+     
+   APP_TransmitMessageQueue(); // Send out CAN FD frame
+}
+
+
+void loop() {
+  uint8_t i;
+  
+  if(digitalRead(JOY_CLICK) == 0)
+  {
+     Serial.println(F("Click"));
+     delay(10);
+  }
+    
+  if(digitalRead(JOY_UP) == 0)
+  {
+     Serial.println(F("up"));
+     delay(50);
+  }
+
+  if(digitalRead(JOY_RIGHT) == 0)
+  {
+     Serial.println(F("RIGHT"));
+     delay(50);
+  }
+
+  if(digitalRead(JOY_LEFT) == 0)
+  {
+     Serial.println(F("LEFT"));
+     delay(50);
+  }
+  
+  if(digitalRead(JOY_DOWN) == 0)
+  {
+     Serial.println(F("DOWN"));
+     delay(50);
+  }
+
+  
+  APP_TransmitMessageQueue(); // Send out CAN FD frame
+
+  txd[4] = can_msg_count >> 24;
+  txd[5] = can_msg_count >> 16;
+  txd[6] = can_msg_count >> 8;
+  txd[7] = can_msg_count;
+
+  can_msg_count++;
+ 
+  delay(500);
+
+  if(digitalReadFast(int1_pin) == 0)  // Read int1 pin on MCP2517FD
+  {
+    // It is low read out the data
+    APP_ReceiveMessage_Tasks();
+  }
+
+}
+
+
+
+
+
+
---
+++ drv_spi.cpp
@@ -0,0 +1,84 @@
+
+#include <SPI.h> 
+#include "drv_spi.h"
+
+
+#include "drv_canfdspi_api.h"
+// Receive Channels
+#define APP_RX_FIFO CAN_FIFO_CH1
+const int test_pin = 23; 
+
+SPISettings settingsA(20000000, MSBFIRST, SPI_MODE0);
+
+void DRV_SPI_Initialize()
+{
+   pinMode (int_pin, INPUT);
+   pinMode (int0_pin, INPUT);
+   pinMode (int1_pin, INPUT);
+   pinMode (slaveSelectPin, OUTPUT);
+   
+   digitalWrite(slaveSelectPin,HIGH); 
+
+   SPI.setMISO(12);       // SCK on pin 14
+   SPI.setSCK(13);       // SCK on pin 13
+   SPI.setMOSI(11);
+    
+   SPI.begin();
+  // SPI.setClockDivider(SPI_CLOCK_DIV2);  
+   Serial.println(F("DRV_SPI_Initialize"));
+   
+   pinMode (test_pin, OUTPUT);
+   digitalWriteFast(test_pin,HIGH);
+}
+
+int8_t DRV_SPI_TransferData(uint8_t spiSlaveDeviceIndex, uint8_t *SpiTxData, uint8_t *SpiRxData, uint16_t spiTransferSize)
+{
+  uint8_t i;
+     
+  SPI.beginTransaction(settingsA);
+  digitalWriteFast(slaveSelectPin,LOW);
+  
+  for(i=0;i<spiTransferSize;i++)
+  {
+      SpiRxData[i] = SPI.transfer(SpiTxData[i]); 
+  }
+  
+  digitalWriteFast(slaveSelectPin,HIGH);
+  SPI.endTransaction();
+  
+  return 0;
+   
+}
+
+CAN_RX_FIFO_EVENT rxFlags;
+extern CAN_RX_MSGOBJ rxObj;
+extern uint8_t rxd[MAX_DATA_BYTES];
+
+
+void APP_ReceiveMessage_Tasks(void)
+{
+
+
+  uint8_t dlc,i,rxstep;
+ 
+    DRV_CANFDSPI_ReceiveChannelEventGet(DRV_CANFDSPI_INDEX_0, APP_RX_FIFO, &rxFlags);
+    if (rxFlags & CAN_RX_FIFO_NOT_EMPTY_EVENT)
+    {
+      // Read the message
+      DRV_CANFDSPI_ReceiveMessageGet(DRV_CANFDSPI_INDEX_0, APP_RX_FIFO, &rxObj, rxd, MAX_DATA_BYTES);
+      dlc = DRV_CANFDSPI_DlcToDataBytes( (CAN_DLC) rxObj.bF.ctrl.DLC);
+     // out_usb();
+      digitalWriteFast(test_pin,LOW);
+    // Serial.print(rxObj.bF.id.SID, HEX);
+     Serial.print(' '); 
+      for(i=0;i<dlc;i++)
+      {
+        Serial.print(rxd[i], HEX);
+      }
+       Serial.println(' '); 
+        digitalWriteFast(test_pin,HIGH);
+    }
+
+
+}
+
